0df5683dfb671afa09b035c6fe974e7b85574787
rowsred <fadlidev99@gmail.com>
2026-04-20 05:07:56 +0000

    kwm config

--- modules/home/dotfiles-manager.nix
+++ modules/home/dotfiles-manager.nix
@@ -6,7 +6,7 @@
 {
   flake.modules.homeManager.dotfiles-manager = {
     xdg.configFile."nvim/init.lua".source = "${inputs.self}/src/config/nvim/init.lua";
-    xdg.configFile."rill/config.zon".source = "${inputs.self}/src/config/rill/config.zon";
+    xdg.configFile."kwm/config.zon".source = "${inputs.self}/src/config/kwm/config.zon";
     xdg.configFile."kitty/kitty.conf".source = "${inputs.self}/src/config/kitty/kitty.conf";
   };
 }
